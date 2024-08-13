e3774533ed47511f1f6e7a41521d6d82dc84ae0b
Ross Cohen <rosscohen1990@gmail.com>
2024-08-12 20:09:35 -0400

    Update page.tsx

--- app/(dashboard)/console/settings/page.tsx
+++ app/(dashboard)/console/settings/page.tsx
@@ -312,7 +312,7 @@ export default async function Settings() {
         </div>
       </PageSection>
 
-{/*       <PageSection className="overflow-y-scroll">
+      <PageSection className="overflow-y-scroll">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none p-6">
           <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-200">
             Bring your own key (beta)
@@ -353,7 +353,7 @@ export default async function Settings() {
             </div>
           </dl>
         </div>
-      </PageSection> */}
+      </PageSection>
     </>
   );
 }
