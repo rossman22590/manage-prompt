5922cfd3294579e95b33d62eb6054c6b413dfc8a
Ross Cohen <rosscohen1990@gmail.com>
2024-11-19 03:06:38 -0500

    update

--- data/workflow.ts
+++ data/workflow.ts
@@ -3,7 +3,7 @@ export const AIModelToLabel = {
   // "gpt-3.5-turbo-0125": "GPT-3.5 Turbo 0125 (Deprecated)",
   // "gpt-4-1106-preview": "GPT-4 1106 (Deprecated)",
   // "gpt-4-0125-preview": "GPT-4 0125 (Deprecated)",
-  "gpt-4": "GPT-4",
+  // "gpt-4": "GPT-4",
   "gpt-4o": "GPT-4o",
   "gpt-4o-mini": "GPT-4o mini",
   "meta-llama/Llama-2-70b-chat-hf": "Meta Llama 2 70b",
@@ -18,8 +18,8 @@ export const modelToProviderId: Record<string | AIModel, string> = {
   "mistralai/Mixtral-8x7B-Instruct-v0.1": "mixtral-8x7b-32768",
   "gpt-3.5-turbo": "gpt-35-turbo",
   "gpt-3.5-turbo-0125": "gpt-35-turbo",
-  "gpt-4-1106-preview": "gpt-4",
-  "gpt-4-0125-preview": "gpt-4",
+  // "gpt-4-1106-preview": "gpt-4",
+  // "gpt-4-0125-preview": "gpt-4",
 };
 
 export const modelToProvider: Record<
@@ -30,7 +30,7 @@ export const modelToProvider: Record<
   // "gpt-3.5-turbo-0125": "openai",
   // "gpt-4-1106-preview": "openai",
   // "gpt-4-0125-preview": "openai",
-  "gpt-4": "openai",
+  // "gpt-4": "openai",
   "gpt-4o": "openai",
   "gpt-4o-mini": "openai",
   "meta-llama/Llama-2-70b-chat-hf": "groq",
