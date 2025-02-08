3101300e6bf361a7dc11c0435038ad937240f656
Ross Cohen <rosscohen1990@gmail.com>
2025-02-08 17:00:35 -0500

    Update page.tsx

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -312,7 +312,7 @@ export default async function Settings() {
       {/* <PageSection className="overflow-y-scroll">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none p-6">
           <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-200">
-            Bring your own key (beta)
+            Bring your own key (beta) 
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-500 dark:text-gray-400">
             If you have your own API key, you can use it to authenticate with AI
