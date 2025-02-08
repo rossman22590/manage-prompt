1fa07292459a7215a14da594c04b95895e79cd14
Ross Cohen <rosscohen1990@gmail.com>
2025-02-08 16:06:17 -0500

    Update page.tsx

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -19,7 +19,6 @@ import {
   getUpcomingInvoice,
   isSubscriptionCancelled,
 } from "@/lib/utils/stripe";
-import { CheckCircle } from "lucide-react";
 import { notFound } from "next/navigation";
 import type Stripe from "stripe";
 import {
@@ -27,12 +26,10 @@ import {
   redirectToBilling,
   removeSpendLimit,
   revokeSecretKey,
-  revokeUserKey,
   updateKeyName,
   updateRateLimit,
   updateSpendLimit,
-  updateUserKey,
-  updateUserName,
+  updateUserName
 } from "./actions";
 
 export default async function Settings() {
@@ -85,7 +82,7 @@ export default async function Settings() {
     ? await getUpcomingInvoice(organization?.stripe?.customerId)
     : null;
 
-  const userOpenAIKey = userKeys.find((k) => k.provider === "openai");
+  // const userOpenAIKey = userKeys.find((k) => k.provider === "openai");
 
   return (
     <>
@@ -312,7 +309,7 @@ export default async function Settings() {
         </div>
       </PageSection>
 
-      <PageSection className="overflow-y-scroll">
+      {/* <PageSection className="overflow-y-scroll">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none p-6">
           <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-200">
             Bring your own key (beta)
@@ -353,7 +350,7 @@ export default async function Settings() {
             </div>
           </dl>
         </div>
-      </PageSection>
+      </PageSection> */}
     </>
   );
 }
