334f0bb2ca958396cc26a811f61a6d80b343dcfc
Ross Cohen <rosscohen1990@gmail.com>
2025-03-05 23:05:12 -0500

    Update workflow.ts

--- data/workflow.ts
+++ data/workflow.ts
@@ -82,7 +82,7 @@ export const modelToProviderId: Record<string | AIModel, string> = {
   "claude-3-opus-20240229": "claude-3-opus-20240229",
   // "claude-3-sonnet-20240229": "claude-3-sonnet-20240229",
   "claude-3-5-sonnet-20241022": "claude-3-5-sonnet-20241022",
-  "claude-3-7-sonnet-20250219": "Claude 3.7 Sonnet",
+  "claude-3-7-sonnet-20250219": "claude-3-7-sonnet-20250219",
   "claude-3-5-haiku-20241022": "claude-3-5-haiku-20241022",
   // Perplexity models
   "sonar-reasoning-pro": "sonar-reasoning-pro",
