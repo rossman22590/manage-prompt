6af98615442654535fe9e6f6236fc60585b250ab
Ross Cohen <rosscohen1990@gmail.com>
2025-03-25 18:16:48 -0400

    Update workflow.ts

--- data/workflow.ts
+++ data/workflow.ts
@@ -30,7 +30,7 @@ export const AIModelToLabel = {
   "claude-3-opus-20240229": "Claude 3 Opus",
   // "claude-3-sonnet-20240229": "Claude 3 Sonnet",
   "claude-3-5-sonnet-20241022": "Claude 3.5 Sonnet",
-  "claude-3-7-sonnet-20250219": "claude-3-7-sonnet-20250219",
+  "claude-3-7-sonnet-20250219": "Claude 3.7 Sonnet",
   "claude-3-5-haiku-20241022": "Claude 3.5 Haiku",
   // Xai models
   "grok-2-latest": "Grok 2",
